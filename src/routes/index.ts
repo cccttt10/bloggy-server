388237d70fb994871062a3a639800e8d06eba249
Chuntong Gao <heihegao@gmail.com>
2020-05-15 00:09:45 -0400

    wrap route handlers in error-handling wrapper

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,17 @@
 import { Express } from 'express';
 
+import { tryAsync } from '../util/util';
 import * as user from './user';
 
-module.exports = (app: Express): void => {
-    app.post('/loginOAuth', user.loginOAuth);
-    app.post('/login', user.login);
-    app.get('/currentUser', user.currentUser);
-    app.post('/logout', user.logout);
-    app.post('/loginAdmin', user.loginAdmin);
-    app.post('/register', user.register);
-    app.post('/delUser', user.delUser);
-    app.get('/getUserList', user.getUserList);
+const setUpRoutes = (app: Express): void => {
+    app.post('/loginOAuth', tryAsync(user.loginOAuth));
+    app.post('/login', tryAsync(user.login));
+    app.get('/currentUser', tryAsync(user.currentUser));
+    app.post('/logout', tryAsync(user.logout));
+    app.post('/loginAdmin', tryAsync(user.loginAdmin));
+    app.post('/register', tryAsync(user.register));
+    app.post('/delUser', tryAsync(user.delUser));
+    app.get('/getUserList', tryAsync(user.getUserList));
 };
+
+export default setUpRoutes;

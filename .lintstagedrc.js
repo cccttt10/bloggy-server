6a648b641171574b2951ac824b1a44c1a8e7a722
Chuntong Gao <heihegao@gmail.com>
2020-05-15 00:07:19 -0400

    change build and lint options

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,10 @@
 module.exports = {
     './src/**/*.js': [
         'prettier --config ./.prettierrc.json --check', 
-        'eslint'
+        'eslint --no-error-on-unmatched-pattern'
     ],
     './src/**/*.ts': [
         'prettier --config ./.prettierrc.json --check', 
-        'eslint',
+        'eslint --no-error-on-unmatched-pattern',
     ]
 };

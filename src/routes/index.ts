072a09b90efbcf2dadcb4c651d1d0841b60f5189
Chuntong Gao <heihegao@gmail.com>
2020-05-15 22:58:04 -0400

    delete unused user routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,10 @@ import { Express } from 'express';
 import user from './user/index';
 
 const setUpRoutes = (app: Express): void => {
-    // app.get('/currentUser', user.currentUser);
     app.post('/deleteAllUsers', user.deleteAllUsers);
-    // app.post('/delUser', user.delUser);
     // app.get('/getUserList', user.getUserList);
     app.post('/getUser', user.getUser);
     app.post('/login', user.login);
-    // app.post('/loginAdmin', user.loginAdmin);
     app.get('/logout', user.logout);
     app.post('/register', user.register);
 };
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,22 +1,16 @@
 import { tryAsync } from '../../util/util';
 import delAllUsers from './delAllUsers';
 import getUser from './getUser';
-// import currentUser from './currentUser';
-// import delUser from './delUser';
 // import getUserList from './getUserList';
 import login from './login';
-// import loginAdmin from './loginAdmin';
 import logout from './logout';
 import register from './register';
 
 export default {
-    // currentUser: tryAsync(currentUser),
     deleteAllUsers: tryAsync(delAllUsers),
     getUser: tryAsync(getUser),
-    // delUser: tryAsync(delUser),
     // getUserList: tryAsync(getUserList),
     login: tryAsync(login),
-    // loginAdmin: tryAsync(loginAdmin),
     logout: tryAsync(logout),
     register: tryAsync(register),
 };

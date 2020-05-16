1eed009ca4b58b49e5d2dd0ee9b3abc5aecda136
Chuntong Gao <heihegao@gmail.com>
2020-05-15 23:11:00 -0400

    delete getUserList route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import user from './user/index';
 
 const setUpRoutes = (app: Express): void => {
     app.post('/deleteAllUsers', user.deleteAllUsers);
-    // app.get('/getUserList', user.getUserList);
     app.post('/getUser', user.getUser);
     app.post('/login', user.login);
     app.get('/logout', user.logout);
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,7 +1,6 @@
 import { tryAsync } from '../../util/util';
 import delAllUsers from './delAllUsers';
 import getUser from './getUser';
-// import getUserList from './getUserList';
 import login from './login';
 import logout from './logout';
 import register from './register';
@@ -9,7 +8,6 @@ import register from './register';
 export default {
     deleteAllUsers: tryAsync(delAllUsers),
     getUser: tryAsync(getUser),
-    // getUserList: tryAsync(getUserList),
     login: tryAsync(login),
     logout: tryAsync(logout),
     register: tryAsync(register),

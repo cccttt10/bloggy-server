50d7cc9c9203314b5eb294922a6fce34b2d5a58d
Chuntong Gao <heihegao@gmail.com>
2020-05-16 17:27:02 -0400

    check for test environment

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
 import request from 'supertest';
 
+describe('environment', () => {
+    it('should run in test environment', () => {
+        expect(process.env.NODE_ENV === 'test');
+    });
+});
+
 describe('/undefined-route', () => {
     const agent = request('http://localhost:3300');
 

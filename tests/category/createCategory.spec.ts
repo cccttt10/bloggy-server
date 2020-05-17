d12b1e084f5ddaab93821e29cdbd834423d61b95
Chuntong Gao <heihegao@gmail.com>
2020-05-16 20:16:53 -0400

    add more tests for category

--- tests/category/createCategory.spec.ts
+++ tests/category/createCategory.spec.ts
@@ -29,24 +29,43 @@ describe('/createCategory', () => {
 
     afterEach(cleanup);
 
-    it('should create a new category for a user', async () => {
+    it('should create new categories for a user', async () => {
         const registerRes = await agent.post('/register').send(users[0]);
         expect(registerRes.body).to.have.property('user');
         expect(registerRes.body.user).to.have.property('_id');
         expect(registerRes.status).to.equal(201);
 
         const userId = registerRes.body.user['_id'];
-        const newCategory: ICategory = { ...categories[0], user: userId };
 
-        const categoryRes = await agent.post('/createCategory').send(newCategory);
-        expect(categoryRes.body).to.have.property('category');
-        const returnedCategory = {
-            name: categoryRes.body.category.name,
-            description: categoryRes.body.category.description,
-            user: categoryRes.body.category.user,
-        };
-        expect(returnedCategory).to.deep.equal(newCategory);
-        expect(categoryRes.status).to.equal(201);
+        {
+            const newCategory: ICategory = { ...categories[0], user: userId };
+            const categoryRes = await agent
+                .post('/createCategory')
+                .send(newCategory);
+            expect(categoryRes.body).to.have.property('category');
+            const returnedCategory = {
+                name: categoryRes.body.category.name,
+                description: categoryRes.body.category.description,
+                user: categoryRes.body.category.user,
+            };
+            expect(returnedCategory).to.deep.equal(newCategory);
+            expect(categoryRes.status).to.equal(201);
+        }
+
+        {
+            const newCategory: ICategory = { ...categories[1], user: userId };
+            const categoryRes = await agent
+                .post('/createCategory')
+                .send(newCategory);
+            expect(categoryRes.body).to.have.property('category');
+            const returnedCategory = {
+                name: categoryRes.body.category.name,
+                description: categoryRes.body.category.description,
+                user: categoryRes.body.category.user,
+            };
+            expect(returnedCategory).to.deep.equal(newCategory);
+            expect(categoryRes.status).to.equal(201);
+        }
     });
 
     it('should return 400 when the same user attempts to create a duplicate category', async () => {

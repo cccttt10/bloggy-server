8ce3c6e7dd4dc390194346b681f4e8bad2373c53
Chuntong Gao <heihegao@gmail.com>
2020-05-22 17:09:32 -0400

    test updateArticle

--- src/routes/article/updateArticle.ts
+++ src/routes/article/updateArticle.ts
@@ -1,12 +1,12 @@
 import { Response } from 'express';
 import { AugmentedRequest } from 'global';
 
-import { Article, ArticleDocument, IArticle } from '../../models/article';
+import { Article, ArticleDocument } from '../../models/article';
 import { MESSAGES } from '../../util/constants';
 import { ServerError } from '../../util/util';
 
 export default async (req: AugmentedRequest, res: Response): Promise<void> => {
-    const { _id } = req.body;
+    const { _id, updatedFields } = req.body;
 
     if (!_id) {
         throw new ServerError({
@@ -31,31 +31,9 @@ export default async (req: AugmentedRequest, res: Response): Promise<void> => {
         });
     }
 
-    const title = req.body.title ? req.body.title : 'Untitled Blog';
-    const description = req.body.description
-        ? req.body.description
-        : 'This is a blog post.';
-    const content = req.body.content ? req.body.content : 'No content yet.';
-    const wordCount =
-        req.body.content && typeof req.body.content === 'string'
-            ? req.body.content.length
-            : 0;
-    const imgUrl = req.body.imgUrl ? req.body.imgUrl : '';
-    const isDraft = req.body.isDraft ? req.body.isDraft : true;
-    const categories = req.body.categories ? req.body.categories : [];
-    const articleInfo: IArticle = {
-        title,
-        author: req.verifiedUser._id,
-        description,
-        content,
-        wordCount,
-        imgUrl,
-        isDraft,
-        categories,
-    };
     const newArticle: ArticleDocument = await Article.findOneAndUpdate(
         _id,
-        articleInfo,
+        { ...updatedFields, updatedOn: Date.now() },
         {
             new: true,
             runValidators: true,
---
+++ test/article/updateArticle.spec.ts
@@ -0,0 +1,231 @@
+/*
+load environment variables
+*/
+require('dotenv').config();
+
+import { expect } from 'chai';
+import request, { SuperTest, Test } from 'supertest';
+
+import App from '../../src/App';
+import { MESSAGES, TEST_SERVER_URL } from '../../src/util/constants';
+import { stdout } from '../../src/util/util';
+import articles from '../test-data/articles';
+import categories from '../test-data/categories';
+import users from '../test-data/users';
+
+describe('/updateArticle', () => {
+    let app: App;
+    let agent: SuperTest<Test>;
+
+    before(() => {
+        app = new App();
+        app.start();
+    });
+
+    after(() => {
+        app.stop();
+    });
+
+    const cleanup = async (): Promise<void> => {
+        let res = await agent.post('/deleteAllUsers').send({
+            sudoSecret: process.env.SUDO_SECRET,
+        });
+        expect(res.status).to.equal(204);
+
+        res = await agent.post('/deleteAllCategories').send({
+            sudoSecret: process.env.SUDO_SECRET,
+        });
+        expect(res.status).to.equal(204);
+
+        res = await agent.post('/deleteAllArticles').send({
+            sudoSecret: process.env.SUDO_SECRET,
+        });
+        expect(res.status).to.equal(204);
+    };
+
+    beforeEach(() => {
+        agent = request(TEST_SERVER_URL);
+        cleanup();
+    });
+
+    afterEach(cleanup);
+
+    it("should an article's author to update the article", async () => {
+        const registerRes = await agent.post('/register').send(users[0]);
+        expect(registerRes.status).to.equal(201);
+
+        const cookie = registerRes.header['set-cookie'];
+
+        const categoryRes0 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[0]);
+        expect(categoryRes0.status).to.equal(201);
+        const categoryId0 = categoryRes0.body.category._id;
+
+        const categoryRes1 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[1]);
+        expect(categoryRes1.status).to.equal(201);
+        const categoryId1 = categoryRes1.body.category._id;
+
+        const articleRes = await agent
+            .post('/createArticle')
+            .set('Cookie', cookie)
+            .send({
+                ...articles[0],
+                categories: [categoryId0],
+            });
+        expect(articleRes.status).to.equal(201);
+        expect(articleRes.body).to.have.property('article');
+        const articleId = articleRes.body.article._id;
+
+        const updateRes0 = await agent
+            .post('/updateArticle')
+            .set('Cookie', cookie)
+            .send({
+                _id: articleId,
+                updatedFields: {
+                    title: 'updated title',
+                    categories: [categoryId0, categoryId1],
+                },
+                debug: true,
+            });
+        expect(updateRes0.status).to.equal(200);
+        expect(updateRes0.body).to.have.property('article');
+        expect(updateRes0.body.article.title).to.equal('updated title');
+        expect(updateRes0.body.article.categories).to.be.ofSize(2);
+        stdout.printResponse(updateRes0);
+    });
+
+    it('should return 400 if article id is not provided', async () => {
+        const registerRes = await agent.post('/register').send(users[0]);
+        expect(registerRes.status).to.equal(201);
+
+        const cookie = registerRes.header['set-cookie'];
+
+        const categoryRes0 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[0]);
+        expect(categoryRes0.status).to.equal(201);
+        const categoryId0 = categoryRes0.body.category._id;
+
+        const categoryRes1 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[1]);
+        expect(categoryRes1.status).to.equal(201);
+        const categoryId1 = categoryRes1.body.category._id;
+
+        const articleRes = await agent
+            .post('/createArticle')
+            .set('Cookie', cookie)
+            .send({
+                ...articles[0],
+                categories: [categoryId0],
+            });
+        expect(articleRes.status).to.equal(201);
+        expect(articleRes.body).to.have.property('article');
+
+        const updateRes0 = await agent
+            .post('/updateArticle')
+            .set('Cookie', cookie)
+            .send({
+                updatedFields: {
+                    title: 'updated title',
+                    categories: [categoryId0, categoryId1],
+                },
+            });
+        expect(updateRes0.status).to.equal(400);
+        expect(updateRes0.body.message).to.equal(MESSAGES.ARTICLE_ID_NOT_PROVIDED);
+    });
+
+    it('should return 400 if article id does not exist', async () => {
+        const registerRes = await agent.post('/register').send(users[0]);
+        expect(registerRes.status).to.equal(201);
+
+        const cookie = registerRes.header['set-cookie'];
+
+        const categoryRes0 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[0]);
+        expect(categoryRes0.status).to.equal(201);
+        const categoryId0 = categoryRes0.body.category._id;
+
+        const categoryRes1 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[1]);
+        expect(categoryRes1.status).to.equal(201);
+        const categoryId1 = categoryRes1.body.category._id;
+
+        const articleRes = await agent
+            .post('/createArticle')
+            .set('Cookie', cookie)
+            .send({
+                ...articles[0],
+                categories: [categoryId0],
+            });
+        expect(articleRes.status).to.equal(201);
+        expect(articleRes.body).to.have.property('article');
+        const articleId = articleRes.body.article._id;
+
+        const updateRes0 = await agent
+            .post('/updateArticle')
+            .set('Cookie', cookie)
+            .send({
+                _id: articleId + 'no such article id',
+                updatedFields: {
+                    title: 'updated title',
+                    categories: [categoryId0, categoryId1],
+                },
+            });
+        expect(updateRes0.status).to.equal(400);
+        expect(updateRes0.body.message).to.equal(MESSAGES.ARTICLE_ID_NOT_FOUND);
+    });
+
+    it('should return 401 if someone attempts to update an article without logging in', async () => {
+        const registerRes = await agent.post('/register').send(users[0]);
+        expect(registerRes.status).to.equal(201);
+
+        const cookie = registerRes.header['set-cookie'];
+
+        const categoryRes0 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[0]);
+        expect(categoryRes0.status).to.equal(201);
+        const categoryId0 = categoryRes0.body.category._id;
+
+        const categoryRes1 = await agent
+            .post('/createCategory')
+            .set('Cookie', cookie)
+            .send(categories[1]);
+        expect(categoryRes1.status).to.equal(201);
+        const categoryId1 = categoryRes1.body.category._id;
+
+        const articleRes = await agent
+            .post('/createArticle')
+            .set('Cookie', cookie)
+            .send({
+                ...articles[0],
+                categories: [categoryId0],
+            });
+        expect(articleRes.status).to.equal(201);
+        expect(articleRes.body).to.have.property('article');
+        const articleId = articleRes.body.article._id;
+
+        const updateRes0 = await agent.post('/updateArticle').send({
+            _id: articleId,
+            updatedFields: {
+                title: 'updated title',
+                categories: [categoryId0, categoryId1],
+            },
+        });
+        expect(updateRes0.status).to.equal(401);
+        expect(updateRes0.body.message).to.equal(MESSAGES.NOT_LOGGED_IN);
+    });
+});

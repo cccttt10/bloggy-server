94fde9f3635fc437139c8ffbf77a2675ae630b46
Chuntong Gao <heihegao@gmail.com>
2020-08-04 11:07:09 -0700

    update front end

---
+++ reader/precache-manifest.9753efec0a80200e79eef3ff89c78fe2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23af0f27b424b25700d5",
+    "url": "/reader/static/js/runtime~main.23af0f27.js"
+  },
+  {
+    "revision": "579152f2b0c027bb5c02",
+    "url": "/reader/static/js/main.579152f2.chunk.js"
+  },
+  {
+    "revision": "b73d18978dde6aec3634",
+    "url": "/reader/static/js/1.b73d1897.chunk.js"
+  },
+  {
+    "revision": "579152f2b0c027bb5c02",
+    "url": "/reader/static/css/main.c5ab7fe1.chunk.css"
+  },
+  {
+    "revision": "a2497d5c75f1b7d4bd3888386f7d2680",
+    "url": "/reader/index.html"
+  }
+];
